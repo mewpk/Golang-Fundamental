4d358b1e842cea32050815a0d4f551e4f384515a
MewPK <milzaza123@gmail.com>
2022-12-25 03:11:03 +0700

    Successful variavle!

--- Stage1/example.go
+++ Stage1/example.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	// ---------- variable ------------------
+	var name string
+	name = "Mew PK" // Manual Type Declaration
+	age := 200      // Type Inference
+	var score float32 = 21.321
+	const codeStatus string = "404"
+	fmt.Println(name, age, score, codeStatus)
+	fmt.Printf("My name is %v \n", name) // %v = value
+	fmt.Printf("Score is %T", score)     // %T = type
+
 }
