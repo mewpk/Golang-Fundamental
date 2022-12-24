a39c0cf1f2f688f2ad8bfb792f69a81335f3cf23
MewPK <milzaza123@gmail.com>
2022-12-25 03:13:58 +0700

    Successful input!

--- Stage1/example.go
+++ Stage1/example.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	// ---------- variable ------------------
+	// ---------- Variable ------------------
 	var name string
 	name = "Mew PK" // Manual Type Declaration
 	age := 200      // Type Inference
@@ -11,6 +11,11 @@ func main() {
 	const codeStatus string = "404"
 	fmt.Println(name, age, score, codeStatus)
 	fmt.Printf("My name is %v \n", name) // %v = value
-	fmt.Printf("Score is %T", score)     // %T = type
+	fmt.Printf("Score is %T \n", score)  // %T = type
+	// ------------- Input ---------------
+	var point int
+	fmt.Print("type ur point:")
+	fmt.Scanf("%d", &point)
+	fmt.Println("Point = ", point)
 
 }
